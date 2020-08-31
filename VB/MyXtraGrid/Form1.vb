ce9175423ef21e830422a3cba6c227adc14a0b51
DevExpressExampleBot <fakeEmail>
2020-08-31 21:25:22 +0300

    Source auto update [skip ci]

--- VB/MyXtraGrid/Form1.vb
+++ VB/MyXtraGrid/Form1.vb
@@ -8,26 +8,26 @@ Imports System.Windows.Forms
 Imports DevExpress.XtraGrid.Controls
 
 Namespace MyXtraGrid
-	Partial Public Class Form1
-		Inherits Form
+    Partial Public Class Form1
+        Inherits Form
 
-		Private Function CreateTable(ByVal RowCount As Integer) As DataTable
-			Dim tbl As New DataTable()
-			tbl.Columns.Add("Name", GetType(String))
-			tbl.Columns.Add("ID", GetType(Integer))
-			tbl.Columns.Add("Number", GetType(Integer))
-			tbl.Columns.Add("Date", GetType(DateTime))
-			For i As Integer = 0 To RowCount - 1
-				tbl.Rows.Add(New Object() { String.Format("Name{0}", i), i, 3 - i, DateTime.Now.AddDays(i) })
-			Next i
-			Return tbl
-		End Function
+        Private Function CreateTable(ByVal RowCount As Integer) As DataTable
+            Dim tbl As New DataTable()
+            tbl.Columns.Add("Name", GetType(String))
+            tbl.Columns.Add("ID", GetType(Integer))
+            tbl.Columns.Add("Number", GetType(Integer))
+            tbl.Columns.Add("Date", GetType(Date))
+            For i As Integer = 0 To RowCount - 1
+                tbl.Rows.Add(New Object() { String.Format("Name{0}", i), i, 3 - i, Date.Now.AddDays(i) })
+            Next i
+            Return tbl
+        End Function
 
 
-		Public Sub New()
-			InitializeComponent()
-			myGridControl1.DataSource = CreateTable(20)
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            myGridControl1.DataSource = CreateTable(20)
+        End Sub
 
-	End Class
+    End Class
 End Namespace
--- VB/MyXtraGrid/MyFindControl.vb
+++ VB/MyXtraGrid/MyFindControl.vb
@@ -9,46 +9,46 @@ Imports DevExpress.XtraLayout
 Imports DevExpress.Utils
 
 Namespace MyXtraGrid
-	Public Class MyFindControl
-		Inherits FindControl
+    Public Class MyFindControl
+        Inherits FindControl
 
-		Public Sub New(ByVal view As ColumnView, ByVal properties As Object)
-			MyBase.New(view, properties)
-			CustomizeControl()
-		End Sub
+        Public Sub New(ByVal view As ColumnView, ByVal properties As Object)
+            MyBase.New(view, properties)
+            CustomizeControl()
+        End Sub
 
 
-		Private Sub CustomizeControl()
-			CustomizeButtons()
-			CustomizeEditor()
-			CustomizeLayoutControl()
-		End Sub
-		Private Function FindControl(ByVal controlName As String) As Control
-			Return layoutControl1.GetControlByName(controlName)
-		End Function
-		Private Sub CustomizeButtons()
-			FindControl("btClear").MinimumSize = New Size(100, 0)
-			FindControl("btClear").Font = New Font(AppearanceObject.DefaultFont, FontStyle.Strikeout)
-			FindControl("btFind").MinimumSize = New Size(100, 0)
-			FindControl("btFind").Font = New Font(AppearanceObject.DefaultFont, FontStyle.Bold)
-		End Sub
-		Private Sub CustomizeEditor()
-			Dim be As ButtonEdit = TryCast(FindControl("teFind"), ButtonEdit)
-			be.Properties.Buttons.Add(New EditorButton(ButtonPredefines.Ellipsis))
-			AddHandler be.ButtonClick, AddressOf be_ButtonClick
-			be.Properties.NullValuePrompt = "MyCustomPanel"
-			be.Properties.NullValuePromptShowForEmptyValue = True
-		End Sub
+        Private Sub CustomizeControl()
+            CustomizeButtons()
+            CustomizeEditor()
+            CustomizeLayoutControl()
+        End Sub
+        Private Function FindControl(ByVal controlName As String) As Control
+            Return layoutControl1.GetControlByName(controlName)
+        End Function
+        Private Sub CustomizeButtons()
+            FindControl("btClear").MinimumSize = New Size(100, 0)
+            FindControl("btClear").Font = New Font(AppearanceObject.DefaultFont, FontStyle.Strikeout)
+            FindControl("btFind").MinimumSize = New Size(100, 0)
+            FindControl("btFind").Font = New Font(AppearanceObject.DefaultFont, FontStyle.Bold)
+        End Sub
+        Private Sub CustomizeEditor()
+            Dim be As ButtonEdit = TryCast(FindControl("teFind"), ButtonEdit)
+            be.Properties.Buttons.Add(New EditorButton(ButtonPredefines.Ellipsis))
+            AddHandler be.ButtonClick, AddressOf be_ButtonClick
+            be.Properties.NullValuePrompt = "MyCustomPanel"
+            be.Properties.NullValuePromptShowForEmptyValue = True
+        End Sub
 
-		Private Sub be_ButtonClick(ByVal sender As Object, ByVal e As ButtonPressedEventArgs)
-			If e.Button.Kind = ButtonPredefines.Ellipsis Then
-				Using form As New Form()
-					form.ShowDialog()
-				End Using
-			End If
-		End Sub
-		Private Sub CustomizeLayoutControl()
-			layoutControl1.AllowCustomizationMenu = True
-		End Sub
-	End Class
+        Private Sub be_ButtonClick(ByVal sender As Object, ByVal e As ButtonPressedEventArgs)
+            If e.Button.Kind = ButtonPredefines.Ellipsis Then
+                Using form As New Form()
+                    form.ShowDialog()
+                End Using
+            End If
+        End Sub
+        Private Sub CustomizeLayoutControl()
+            layoutControl1.AllowCustomizationMenu = True
+        End Sub
+    End Class
 End Namespace
