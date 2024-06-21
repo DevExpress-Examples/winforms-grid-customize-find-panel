96f3f3fc5f2b03e5b95199bda49bc2e0d7f3bcf1
NadezhdaDX <nadezhda.bobrova@devexpress.com>
2024-06-21 16:13:58 +0400

    Use FindPanelItems

--- CS/MyXtraGrid/Form1.cs
+++ CS/MyXtraGrid/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using DevExpress.XtraEditors;
 using DevExpress.XtraGrid.Controls;
 
 namespace MyXtraGrid {
